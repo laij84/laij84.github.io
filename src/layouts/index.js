d9193e733cf78378a7c516e9b264eb91bf24f8a8
Jason Lai <jase_lai@hotmail.com>
2018-01-28 21:36:12 +0000

    blogpost banner styles added, nav toggle working

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,59 +8,81 @@ import PageTransition from '../components/PageTransition'
 import './index.css'
 import '../assets/scss/style.scss'
 
-const Nav = () => (
-    <nav className="nav">
-        <ul className="nav__list">
-            <li className="nav__listItem">
-                <Link className="nav__link" to="/">
-                    <i className="icon-home"></i>
-                    <span className="nav__linkTitle">Home</span>
-                </Link>
-            </li>
-            <li className="nav__listItem">
-                <Link className="nav__link" to="/">
-                    <i className="icon-user"></i>
-                    <span className="nav__linkTitle">About</span>
-                </Link>
-            </li>
-            <li className="nav__listItem">
-                <Link className="nav__link" to="/">
-                    <i className="icon-code"></i>
-                    <span className="nav__linkTitle">Work</span>
-                </Link>
-            </li>
-            <li className="nav__listItem">
-                <Link className="nav__link" to="/">
-                    <i className="icon-blog"></i>
-                    <span className="nav__linkTitle">Blog</span>
-                </Link>
-            </li>
-            <li className="nav__listItem">
-                <Link className="nav__link" to="/">
-                    <i className="icon-mail"></i>
-                    <span className="nav__linkTitle">Contact</span>
-                </Link>
-            </li>
-        </ul>
-        <ul className="nav__list -social">
-            <li className="nav__listItem -social">
-                <a href="https://twitter.com/LaiJase" className="nav__link" to="/">
-                    <i className="icon-twitter"></i>
-                </a>
-            </li>
-            <li className="nav__listItem -social"><i className="icon-github"></i></li>
-            <li className="nav__listItem -social"><i className="icon-linkedin"></i></li>
-        </ul>
-    </nav>
-)
-
 class TemplateWrapper extends Component {
     constructor() {
         super()
         this.state = {
-          in: true
+          in: true,
+          navOpen: false
         }
     }
+
+    toggleNav = () => {
+        this.setState({navOpen: !this.state.navOpen});
+        console.log(this.state.navOpen);
+    }
+
+    renderNav = () => {
+        return (
+            <nav className={this.state.navOpen ? 'nav --open' : 'nav'}>
+                <ul className="nav__list">
+                    <li className="nav__listItem">
+                        <Link className="nav__link" to="/">
+                            <i className="icon-home"></i>
+                            <span className="nav__linkTitle">Home</span>
+                        </Link>
+                    </li>
+                    <li className="nav__listItem">
+                        <Link className="nav__link" to="/">
+                            <i className="icon-user"></i>
+                            <span className="nav__linkTitle">About</span>
+                        </Link>
+                    </li>
+                    <li className="nav__listItem">
+                        <Link className="nav__link" to="/">
+                            <i className="icon-code"></i>
+                            <span className="nav__linkTitle">Work</span>
+                        </Link>
+                    </li>
+                    <li className="nav__listItem">
+                        <Link className="nav__link" to="/">
+                            <i className="icon-blog"></i>
+                            <span className="nav__linkTitle">Blog</span>
+                        </Link>
+                    </li>
+                    <li className="nav__listItem">
+                        <Link className="nav__link" to="/">
+                            <i className="icon-mail"></i>
+                            <span className="nav__linkTitle">Contact</span>
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="nav__list -social">
+                    <li className="nav__listItem -social">
+                        <a href="https://twitter.com/LaiJase" className="nav__link" to="/">
+                            <i className="icon-twitter"></i>
+                            <span className="nav__linkTitle -social">Twitter</span>
+                        </a>
+                    </li>
+                    <li className="nav__listItem -social">
+                        <a href="https://github.com/laij84" className="nav__link" to="/">
+                            <i className="icon-github"></i>
+                            <span className="nav__linkTitle -social">Github</span>
+                        </a>
+                    </li>
+                    <li className="nav__listItem -social">
+                        <a href="https://twitter.com/LaiJase" className="nav__link" to="/">
+                            <i className="icon-linkedin"></i>
+                            <span className="nav__linkTitle -social">Linkedin</span>
+                        </a>
+                    </li>
+                </ul>
+                <div className="nav__mobileToggle" onClick={this.toggleNav}>
+                    <span className="nav__mobileToggleIcon"></span>
+                </div>
+            </nav>
+        )
+    } 
 
     render() {
         const getTransitionState = this.getTransitionState;
@@ -73,7 +95,7 @@ class TemplateWrapper extends Component {
                 { name: 'keywords', content: 'sample, something' },
                 ]}
             />
-            <Nav />
+            {this.renderNav()}
             <TransitionGroup>
                 {this.props.children({...this.props, getTransitionState})}
             </TransitionGroup>
--- src/templates/post.js
+++ src/templates/post.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 import PageTransition from '../components/PageTransition'
 import hljs from 'highlight.js'
+import moment from 'moment'
 
 export default class PostPage extends Component {
     constructor() {
@@ -10,7 +11,6 @@ export default class PostPage extends Component {
     }
 
     componentDidMount() {
-        // hljs.initHighlightingOnLoad();
         let codeblocks = this.content.querySelectorAll('pre code');
         codeblocks.forEach((block) => {
             hljs.highlightBlock(block);
@@ -23,20 +23,46 @@ export default class PostPage extends Component {
 
     render() {
         const post = this.props.data.contentfulPost; 
+        console.log(post);
         return (
             <PageTransition
                 timeout={350}
                 classNames="fade"
                 shouldShow={this.state.in}
             >
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-10 offset-sm-1" ref={(elem) => { this.content = elem; }}>
-                        <h1>{post.title}</h1>
-                        <ReactMarkdown source={post.content.content} escapeHtml={false}/>
+                <div className="pageWrap">
+                    <div className="banner" style={{backgroundImage: `url(${post.banner.file.url})`}}>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-8 col-sm-10 offset-sm-1">
+                                    <div className="banner__cont">
+                                        <p className="banner__tags">
+                                            <small>
+                                                <i className="icon-tag"></i>&nbsp;
+                                                {post.tags.map((tag, index)=>{
+                                                    return post.tags.length !== (index + 1) ? `${tag.name}, ` : `${tag.name}` 
+                                                })}
+                                            </small>
+                                        </p>
+                                        <h1 className="banner__title">{post.title}</h1>
+                                        <ReactMarkdown className="banner__intro" source={post.intro.intro} escapeHtml={false}/>
+                                        <p className="banner__published"><small>Posted {moment(post.published).fromNow()}</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="banner__ovly"></div>
+                    </div>
+                    <div className="section">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-sm-10 offset-sm-1" ref={(elem) => { this.content = elem; }}>
+                                    <ReactMarkdown source={post.content.content} escapeHtml={false}/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
             </PageTransition>
         )
     }
@@ -47,6 +73,14 @@ query PostPageQuery ($id: String!) {
     contentfulPost(slug: {eq: $id}) {
         id
         title
+        banner {
+          id
+          file {
+            url
+            fileName
+            contentType
+          }
+        }
         published
         slug
         categories {
@@ -58,8 +92,11 @@ query PostPageQuery ($id: String!) {
           name
         }
         content {
-              content
-          }
+          content
+        }
+        intro {
+            intro
+        }
     }
 }
 `
